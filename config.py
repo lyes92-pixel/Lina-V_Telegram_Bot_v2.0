898a087c85ca421c5bfef8f3269595a993fdb4a1
lyes92-pixel <benamrane.lyes92@gmail.com>
2025-10-25 13:02:25 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 1234567
+API_HASH = "your_api_hash_here"
+BOT_TOKEN = "your_bot_token_here"
+
+# Google Sheets إعدادات
+SHEET_ID = "your_google_sheet_id_here"
+GOOGLE_CREDENTIALS_FILE = "credentials.json"

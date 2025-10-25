efcd7774ba8cf148ff2693e8456f36337c8f364e
lyes92-pixel <benamrane.lyes92@gmail.com>
2025-10-25 14:20:45 +0100

    Initial commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 1234567
-API_HASH = "your_api_hash_here"
-BOT_TOKEN = "your_bot_token_here"
+API_ID = 21340744
+API_HASH = "21340744"
+BOT_TOKEN = "92acf8436a55be7fba2cb5aa962f2338"
 
 # Google Sheets إعدادات
-SHEET_ID = "your_google_sheet_id_here"
+SHEET_ID = "1xKH1y3BtkW-yDkdxwojyivzNxJxLmcP4efgTEQPETwA"
 GOOGLE_CREDENTIALS_FILE = "credentials.json"

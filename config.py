c56e24e151f090d5b988b2fd7dd47383eb0f6fe4
lyes92-pixel <benamrane.lyes92@gmail.com>
2025-10-26 13:18:31 +0100

    Initial commit of MMG Bot

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 API_ID = 21340744
-API_HASH = "21340744"
-BOT_TOKEN = "92acf8436a55be7fba2cb5aa962f2338"
+API_HASH = "92acf8436a55be7fba2cb5aa962f2338"
+BOT_TOKEN = "8488526278:AAFcxne28UEETEwFAKbr_LOhNQeEFfmjLC8"
 
 # Google Sheets إعدادات
 SHEET_ID = "1xKH1y3BtkW-yDkdxwojyivzNxJxLmcP4efgTEQPETwA"
 GOOGLE_CREDENTIALS_FILE = "credentials.json"
+
+LEVEL1_LINK = "https://t.me/InVisionPro"
